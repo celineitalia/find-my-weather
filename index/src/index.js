6118dac814d15fb2a1faa7fe77c44af09c8b1af0
Celine Italia <celineitalia@yahoo.com>
2021-06-04 12:47:04 -0500

    JS Search Engine Changes

--- index/src/index.js
+++ index/src/index.js
@@ -57,9 +57,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "be8543105d666be03c84a05e1723a6aa";
-let city = "Milwaukee";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "be8543105d666be03c84a05e1723a6aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-text-input");
+  search(cityInputElement.value);
+}
+
+search("Milwaukee");
+
+let form = document.querySelector("#city-input");
+form.addEventListener("submit", handleSubmit);
