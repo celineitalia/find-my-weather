8f3fbfee6c78ee40e867ab1e7404b5e229390f27
Celine Italia <celineitalia@yahoo.com>
2021-06-04 16:35:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function formatDate(timestamp) {
   return `${day}, ${month} ${date}, ${year} | ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
@@ -56,6 +56,12 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "be8543105d666be03c84a05e1723a6aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -76,6 +82,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -115,4 +123,3 @@ let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", displayCelsiusTemp);
 
 search("Milwaukee");
-displayForecast();
