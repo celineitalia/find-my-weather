8a2287cc3edf33ccb48338bfddc44ee5715592a7
Celine Italia <celineitalia@yahoo.com>
2021-06-04 12:27:00 -0500

    Added Weather Icon API

--- index/src/index.js
+++ index/src/index.js
@@ -44,11 +44,17 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "be8543105d666be03c84a05e1723a6aa";
