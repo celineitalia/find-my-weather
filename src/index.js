b7eaf6dee0d08f449a868691051b04557bcd2884
Celine Italia <celineitalia@yahoo.com>
2021-06-04 16:22:25 -0500

    Added JS Forecast

--- src/index.js
+++ src/index.js
@@ -38,6 +38,26 @@ function formatDate(timestamp) {
   return `${day}, ${month} ${date}, ${year} | ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+          <div class="forecast-date">${day}</div>
+          <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="" />
+          <div class="forcast-temp">
+            <span class="forecast-high">18°</span
+            ><span class="forecast-low">12°</span>
+          </div>
+        </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -95,3 +115,4 @@ let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", displayCelsiusTemp);
 
 search("Milwaukee");
+displayForecast();
