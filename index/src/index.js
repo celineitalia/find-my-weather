8878ee08703556267bdd2bbc9f9fb71c42404703
Celine Italia <celineitalia@yahoo.com>
2021-06-04 12:08:21 -0500

    JS Time Changes

--- index/src/index.js
+++ index/src/index.js
@@ -1,13 +1,55 @@
+function formatDate(timestamp) {
+  let dates = new Date(timestamp);
+  let hours = dates.getHours();
+  let minutes = dates.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dates.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[dates.getMonth()];
+  let date = dates.getDate();
+  let year = dates.getFullYear();
+  return `${day}, ${month} ${date}, ${year} | ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "be8543105d666be03c84a05e1723a6aa";
