d98fbab55e4140c30f3cc382456826d863b63946
Celine Italia <celineitalia@yahoo.com>
2021-06-04 16:53:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,21 +38,36 @@ function formatDate(timestamp) {
   return `${day}, ${month} ${date}, ${year} | ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-          <div class="forecast-date">${day}</div>
-          <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="" />
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+          <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png" alt="" />
           <div class="forcast-temp">
-            <span class="forecast-high">18°</span
-            ><span class="forecast-low">12°</span>
+            <span class="forecast-high">${Math.round(
+              forecastDay.temp.max
+            )}°</span
+            >/<span class="forecast-low">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
           </div>
         </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
